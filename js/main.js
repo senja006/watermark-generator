d60c9c38048beae5288b2bd233e87bc8824ea083
Dan Ratnikov <ratnikov.danil@gmail.com>
2015-01-15 01:06:55 +0500

    <Оpaсity> and <Coordinates> under control

--- js/main.js
+++ js/main.js
@@ -2,17 +2,49 @@
 
 (function(){
 
+  var $dragBoxes = $('.drag'),
+      $opacityField = $('#opacity-field'),
+      $opacity = $('#opacity'),
+      $opacityVal = $('#opacity-val'),
+      $xVal = $('#xVal'),
+      $yVal = $('#yVal'),
+      $backgroundURL = $('#backgroundURL'),
+      $watermarkURL = $('#watermarkURL');
   
 	var app = {
-			
-			initialize: function(){
+		
+		initialize: function(){
+			$opacityVal.text($dragBoxes.css('opacity'));
+			$watermarkURL.text($('#drag__img').attr('src'));
+    	$backgroundURL.text($('#bg__img').attr('src'));
+			$dragBoxes.draggable({
+	      cursor: 'move',
+	      snap: '.result',
+	      drag: app.updateCoordFields
+	    });
+	    this.setupListeners();
+		},
 
-			},
+		setupListeners: function(){
+			  $opacity.on('change', this.opacityChange);
+		},
 
-			setupListeners: function(){
+		opacityChange: function(e){
+	    var opacity = $(e.target).val() / 100; 
+	    $dragBoxes.css({
+	      'opacity': opacity
+	    });
+	    $opacityVal.text(opacity);
+		},
 
-			}
-	}
+		updateCoordFields: function(e, ui){
+	    $xVal.text(ui.position.left  + 1);
+	    $yVal.text(ui.position.top + 1);
+	  }
+
+	};
+
+	app.initialize();
 
 }());
 
