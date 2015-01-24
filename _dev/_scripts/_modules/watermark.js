650e38828c94107243accc6ea5b8eecbaf934248
Sergei Yarkevich <senja999@mail.ru>
2015-01-24 12:23:03 +0300

    Рефакторинг js

--- _dev/_scripts/_modules/watermark.js
+++ _dev/_scripts/_modules/watermark.js
@@ -6,44 +6,37 @@ var watermark = (function() {
 		$opacity = $('#opacity'), // ползунок прозрачности		
 		$xVal = $('#x-val'), // поле left
 		$yVal = $('#y-val'), // поле top
-
-		correctionX = 0; // коорекция координат в зависимости от размера картинки
-		correctionY = 0;
-
-	var watermarkWidth = 0,  // размеры
+		correctionX = 0, // коорекция координат в зависимости от размера картинки
+		correctionY = 0,
+		watermarkWidth = 0,  // размеры
 		watermarkHeight = 0,
 		bgWidth = 0,
 		bgHeight = 0,
 		scaleImgX = 1, // масштаб
 		scaleImgY = 1,
-
-		fixedPositions = {}, 
-
 		currPos = {    // текушее положение ватермарки top, left
 			left: 0,
 			top: 0
 		},
-
-		currOpacity = 0.5; // текущая прозрачность
-
-	var optionsDraggable = {
-		cursor: 'move',
-		snap: '#bg__wrapper',
-		drag: onDragWatermark // событие 'drag'
-	};
-	var optionsSliderRange = {
-		animate: true,
-		range: "min",
-		value: 50,
-		min: 0,
-		max: 100,
-		step: 1,
-		slide: onOpacityChange // событине на ползунке
-	};
-	var resultBoxWidth = $('#result-box').width();
-	var resultBoxHeight = $('#result-box').height();
-	var $imgMain = null;
-	var $imgWatermark = null;
+		currOpacity = null, // текущая прозрачность
+		optionsDraggable = {
+			cursor: 'move',
+			snap: '#bg__wrapper',
+			drag: onDragWatermark // событие 'drag'
+		},
+		optionsSliderRange = {
+			animate: true,
+			range: "min",
+			value: 50,
+			min: 0,
+			max: 100,
+			step: 1,
+			slide: onOpacityChange // событине на ползунке
+		},
+		resultBoxWidth = $('#result-box').width(),
+		resultBoxHeight = $('#result-box').height(),
+		$imgMain = null,
+		$imgWatermark = null;
 
 	function addEventListeners() {
 		$watermark.draggable(optionsDraggable);
@@ -78,7 +71,7 @@ var watermark = (function() {
 	function controlGetOptionsImg(){
 		$imgMain = $('#bg__img');
 		$imgWatermark = $('#drag__img');
-		$('#bg__img, #drag__img').on('load.img', function() {
+		$imgMain.add($imgWatermark).on('load.img', function() {
 			getOptionsImgMain();
 			getOptionsWatermark();
 			setCorrectionVariables();
@@ -86,7 +79,7 @@ var watermark = (function() {
 			if(bgHeight > 0 && watermarkHeight > 0) {
 				refreshPosInput();
 			}
-			$('#bg__img, #drag__img').off('load.img');
+			$imgMain.add($imgWatermark).off('load.img');
 		});
 	};
 
@@ -126,39 +119,6 @@ var watermark = (function() {
 			'max-width': watermarkWidth * scaleImgX,
 		});
 	};
-
-	// function getOptionsImg() {
-	// 	$('#bg__img, #drag__img').on('load.img', function() {
-			
-			
-
-
-	// 		console.log('calc');
-	// 		// $('#bg__img, #drag__img').off('load.img');
-			
-	// 		// объект dataCoordinates для отладки координат
-	// 		var dataCoordinates = {
-	// 			'resultBoxWidth': resultBoxWidth,
-	// 			'resultBoxHeight': resultBoxHeight,
-	// 			'bgWidth': bgWidth,
-	// 			'bgHeight': bgHeight,
-	// 			'watermarkWidth': watermarkWidth,
-	// 			'watermarkHeight': watermarkHeight,
-	// 			'correctionX': correctionX,
-	// 			'correctionY': correctionY,
-	// 			'correctionWatermarkWidth': correctionWatermarkWidth,
-	// 			'correctionWatermarkHeight': correctionWatermarkHeight,
-	// 			'scaleImgX': scaleImgX,
-	// 			'scaleImgY': scaleImgY,
-	// 			'max-width': watermarkWidth * correctionWatermarkWidth,
-	// 			'max-height': watermarkHeight * correctionWatermarkHeight
-	// 		}
-	// 		console.log(dataCoordinates);
-	// 		// if(bgHeight > 0 && watermarkHeight > 0) {
-	// 		// 	refreshPosInput();
-	// 		// }
-	// 	});
-	// };
 
 	// обработчик смены прозрачности
 	function onOpacityChange(e, ui) {
