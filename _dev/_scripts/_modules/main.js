c5fbc7185630fba6def53d559d4c929589f9b097
freewayspb <alexey.chyrva@gmail.com>
2015-01-28 08:18:52 +0300

    fix init

--- _dev/_scripts/_modules/main.js
+++ _dev/_scripts/_modules/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     watermark.init();       // управление водяным знаком
     images.init();			// управление картинками
     lang.init();            // Смена языка
-    socialShare.init();     // Кнопки соц сетей
+    //socialShare.init();     // Кнопки соц сетей
 
 });
 
